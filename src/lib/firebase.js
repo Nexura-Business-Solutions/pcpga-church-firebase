da1355828ff026ff6400faf853ccddf683a37c4e
ChimChimmyChim <slowdee59@gmail.com>
2026-05-15 14:20:46 +0800

    feat(lib): firebase init with hardcoded config

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getFunctions } from 'firebase/functions';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCqpSM7AQoaNYq32VxoJ4MvVC8NvL4cmfE',
+  authDomain: 'pcpga-church-prod.firebaseapp.com',
+  projectId: 'pcpga-church-prod',
+  storageBucket: 'pcpga-church-prod.firebasestorage.app',
+  messagingSenderId: '264889432358',
+  appId: '1:264889432358:web:356a6034802347a69e69e7',
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const functions = getFunctions(app, 'asia-southeast1');
