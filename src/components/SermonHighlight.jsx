8714497036e428c782a5bfaf0b02daf133905752
ChimChimmyChim <slowdee59@gmail.com>
2026-05-15 14:48:05 +0800

    chore: replace deprecated frameBorder attr with inline style on iframe

--- src/components/SermonHighlight.jsx
+++ src/components/SermonHighlight.jsx
@@ -78,7 +78,7 @@ export default function SermonHighlight() {
                             <iframe
                                 src={`https://www.youtube.com/embed/${ytId}?autoplay=1`}
                                 title={current?.title || ''}
-                                frameBorder="0"
+                                style={{ border: 0 }}
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 allowFullScreen
                             />
